a2e314707a0448a219ad06f31a0af147ad802882
Angel-M-R <angelmorenoromera@gmail.com>
2023-12-07 13:59:31 +0100

    init vite tailwind

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+
+  return (
+    <>
+      <section>
+        Header
+      </section>
+      <section>
+        
+      </section>
+      <footer>
+        
+      </footer>
+    </>
+  )
+}
+
+export default App
