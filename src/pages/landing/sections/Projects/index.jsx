f620d43157c97b68b51dbf9333084be12671e3d8
Angel-M-R <angelmorenoromera@gmail.com>
2023-12-12 16:37:11 +0100

    start projects section

---
+++ src/pages/landing/sections/Projects/index.jsx
@@ -0,0 +1,15 @@
+import { SectionTitle } from "../../components/SectionTitle"
+
+function Projects() {
+  return (
+    <section className="flex flex-col">
+      <SectionTitle title="Projects"/>
+      <section className="flex items-center flex-col">
+        
+      </section>
+
+    </section>
+  )
+}
+
+export {Projects}
